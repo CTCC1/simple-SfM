19ddeca9e4226165874ed9c4bec3798f489738d3
DeqingFu <deqing@uchicago.edu>
2018-12-10 18:41:22 -0600

    uh

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def int_to_str(n):
   ret = str(n)
   return "0" * (3-len(ret)) + ret
 
-N = 3 # dataset size
+N = 2 # dataset size
 DATA_DIR = "./data/house/images" #data size 10
 CAMERA_DIR = "./data/house/3D" 
 IMAGE_DIRS = [os.path.join(DATA_DIR, "house." + int_to_str(n) + ".pgm" )for n in range(N)]
@@ -21,7 +21,7 @@ images = [load_image(img) for img in IMAGE_DIRS]
 CAMERA_DIRS = [os.path.join(CAMERA_DIR, "house." + int_to_str(n) + ".P") for n in range(N)]
 camera_matrices = [np.loadtxt(cam) for cam in CAMERA_DIRS]
 ItoWs = [np.vstack((m, [[0,0,0,1]])) for m in camera_matrices]
-WtoIs = [m.T for m in ItoWs]
+WtoIs = [np.linalg.inv(m) for m in ItoWs]
 
 transformation_matrices = [np.eye(3,3)]
 matched_points = [] # in its own coordinate
@@ -48,36 +48,36 @@ for i in range(N):
   t = m[:,3:]
   c = -np.matmul(np.linalg.inv(R), t).T[0] # camera center
   cc = np.matmul(transformation_matrices[i], c)
-  camera_centers.append(cc)
-  pts = [np.matmul(transformation_matrices[i], pt) for pt in matched_points[i]]
+  camera_centers.append(cc.reshape((1,3)))
+  pts = [np.matmul(transformation_matrices[i], pt).reshape((1,3)) for pt in matched_points[i]]
   points.append(pts)
 
 
 res_points = []
-for i in range(N-1):
-  pts0 = points[i]
-  pts1 = points[i+1]
-  cc0 = camera_centers[i]
-  cc1 = camera_centers[i+1]
-  eye = np.eye(3)
-  n = min(len(pts0), len(pts1))
-  print(n)
-  for k in range(n):
-    try:
-      x0 = pts0[k]
-      x1 = pts1[k]
-    except:
-      continue
-    v0 = x0 - cc0
-    #v0 = v0/np.linalg.norm(v0)
-    v1 = x1 - cc1
-    #v1 = v1/np.linalg.norm(v1)
-    print(v0, v1)
-    # calculate the intersection point, reference from Book, might be wrong
-    tmp1 = ((eye - np.matmul(v0, v0.T)) + (eye - np.matmul(v1, v1.T))).T
-    tmp2 = np.matmul((eye - np.matmul(v0, v0.T)), cc0) + np.matmul((eye - np.matmul(v1, v1.T)), cc1)
-    point = np.matmul(tmp1, tmp2)
-    res_points.append(point)
+for i in range(N):
+  for j in range(i, N):
+    pts0 = points[i]
+    pts1 = points[j]
+    cc0 = camera_centers[i]
+    cc1 = camera_centers[j]
+    eye = np.eye(3)
+    n = min(len(pts0), len(pts1))
+    print(n)
+    for k in range(n):
+      try:
+        x0 = pts0[k]
+        x1 = pts1[k]
+      except:
+        continue
+      v0 = x0 - cc0
+      v0 = v0/np.linalg.norm(v0)
+      v1 = x1 - cc1
+      v1 = v1/np.linalg.norm(v1)
+      # calculate the intersection point, reference from Book, might be wrong
+      tmp1 = np.linalg.inv(((eye - np.matmul(v0.T, v0)) + (eye - np.matmul(v1.T, v1))))
+      tmp2 = np.matmul((eye - np.matmul(v0.T, v0)), cc0.T) + np.matmul((eye - np.matmul(v1.T, v1)), cc1.T)
+      point = np.matmul(tmp1, tmp2)
+      res_points.append(point)
 
 print(res_points)
 xs = [p[0] for p in res_points]
